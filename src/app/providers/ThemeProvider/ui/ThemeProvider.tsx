452ea126a3874a4774f22303c2a0d551e58b99d2
ltvnkvtl <vital.litovk@gmail.com>
2023-02-04 13:16:10 +0400

    feat: feature sliced design project structure

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,9 +1,9 @@
 import React, {FC, useMemo, useState} from 'react';
-import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from './ThemeContext';
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from '../lib/ThemeContext';
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
-const ThemeProvider: FC = ({ children }) => {
+export const ThemeProvider: FC = ({ children }) => {
   const [theme, setTheme] = useState(defaultTheme);
   const defaultProps = useMemo(() => ({
     theme,
@@ -15,5 +15,3 @@ const ThemeProvider: FC = ({ children }) => {
     </ThemeContext.Provider>
   );
 };
-
-export default ThemeProvider;
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,4 @@
 import path from "path";
-import webpack from "webpack";
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
 import {BuildEnvironment, BuildMode, BuildPaths} from "./config/build/types/config";
 
@@ -9,16 +8,15 @@ export default (env: BuildEnvironment) => {
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
     build: path.resolve(__dirname, 'build'),
     html: path.resolve(__dirname, 'public', 'index.html'),
+    src: path.resolve(__dirname, 'src'),
   }
   const isDev = mode === "development";
   const PORT = env.port || 3000;
 
-  const config: webpack.Configuration = buildWebpackConfig({
+  return buildWebpackConfig({
     mode,
     paths,
     isDev,
     port: PORT,
   });
-
-  return config;
 };
