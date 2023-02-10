c6009396776c4f401211bf3ec888fbadea194544
ltvnkvtl <vital.litovk@gmail.com>
2023-02-10 11:53:12 +0400

    feat(stylelint): add stylelint, i18next linter

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Button, ButtonType } from 'shared/ui/Button/Button';
-import cls from './LangSwitcher.module.scss';
 
 interface LangSwitcherProps {
   className?: string;
@@ -16,7 +15,7 @@ export const LangSwitcher = ({ className }: LangSwitcherProps) => {
 
     return (
         <Button
-            className={classNames(cls.LangSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
             buttonType={ButtonType.CLEAR}
             onClick={toggle}
         >
