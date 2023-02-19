a0bcc42d8c3b87615c9645c2022cc8610a676246
ltvnkvtl <vital.litovk@gmail.com>
2023-02-19 09:23:33 +0400

    feat(button): add new types, sizes, square state, stories

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Button, ButtonType } from './Button';
+import { Button, ButtonSize, ButtonType } from './Button';
 
 export default {
     title: 'shared/Button',
@@ -23,8 +23,58 @@ Clear.args = {
     buttonType: ButtonType.CLEAR,
 };
 
-export const Outline = Template.bind({});
-Outline.args = {
+export const OutlineSizeM = Template.bind({});
+OutlineSizeM.args = {
     children: 'Text',
     buttonType: ButtonType.OUTLINE,
+};
+
+export const OutlineSizeL = Template.bind({});
+OutlineSizeL.args = {
+    children: 'Text',
+    buttonType: ButtonType.OUTLINE,
+    size: ButtonSize.L,
+};
+
+export const OutlineSizeXL = Template.bind({});
+OutlineSizeXL.args = {
+    children: 'Text',
+    buttonType: ButtonType.OUTLINE,
+    size: ButtonSize.XL,
+};
+
+export const Background = Template.bind({});
+Background.args = {
+    children: 'Text',
+    buttonType: ButtonType.BACKGROUND,
+};
+
+export const BackgroundInverted = Template.bind({});
+BackgroundInverted.args = {
+    children: 'Text',
+    buttonType: ButtonType.BACKGROUND_INVERTED,
+};
+
+export const SquareSizeM = Template.bind({});
+SquareSizeM.args = {
+    children: '>',
+    buttonType: ButtonType.BACKGROUND_INVERTED,
+    square: true,
+    // size: ButtonSize.M, // default
+};
+
+export const SquareSizeL = Template.bind({});
+SquareSizeL.args = {
+    children: '>',
+    buttonType: ButtonType.BACKGROUND_INVERTED,
+    square: true,
+    size: ButtonSize.L,
+};
+
+export const SquareSizeXL = Template.bind({});
+SquareSizeXL.args = {
+    children: '>',
+    buttonType: ButtonType.BACKGROUND_INVERTED,
+    square: true,
+    size: ButtonSize.XL,
 };
