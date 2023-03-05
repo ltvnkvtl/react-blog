1f2eec88982fbb9f8d5bc3c00ca3d3d817fcceb8
ltvnkvtl <vital.litovk@gmail.com>
2023-03-05 10:27:13 +0400

    test(storybook): add asyncReducers for store decorator

--- src/app/providers/store/ui/StoreProvider.tsx
+++ src/app/providers/store/ui/StoreProvider.tsx
@@ -2,19 +2,25 @@ import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { createReduxStore } from 'app/providers/store';
 import { StateSchema } from 'app/providers/store/config/StateSchema';
+import { ReducersMapObject } from '@reduxjs/toolkit';
 
 interface StoreProviderProps {
   children?: ReactNode;
   initialState?: StateSchema;
+  asyncReducers?: ReducersMapObject<StateSchema>
 }
 
 export const StoreProvider = (props: StoreProviderProps) => {
     const {
         children,
         initialState,
+        asyncReducers,
     } = props;
 
-    const store = createReduxStore(initialState);
+    const store = createReduxStore(
+        initialState,
+        asyncReducers,
+    );
 
     return (
         <Provider store={store}>
