b5a7d5bda902815148b9e1a2068e852de5d7e561
ltvnkvtl <vital.litovk@gmail.com>
2023-03-08 10:16:59 +0400

    refactor: add memo for components

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,5 +1,5 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { ButtonHTMLAttributes, FC } from 'react';
+import { ButtonHTMLAttributes, memo, ReactNode } from 'react';
 import cls from './Button.module.scss';
 
 export enum ButtonType {
@@ -21,9 +21,10 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     buttonType?: ButtonType;
     square?: boolean;
     size?: ButtonSize;
+    children?: ReactNode;
 }
 
-export const Button: FC<ButtonProps> = (props) => {
+export const Button = memo((props: ButtonProps) => {
     const {
         className,
         children,
@@ -46,4 +47,4 @@ export const Button: FC<ButtonProps> = (props) => {
             {children}
         </button>
     );
-};
+});
--- src/shared/ui/Loader/Loader.tsx
+++ src/shared/ui/Loader/Loader.tsx
@@ -1,13 +1,14 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import './Loader.scss';
+import { memo } from 'react';
 
 interface LoaderProps {
   className?: string;
 }
 
-export const Loader = ({ className }: LoaderProps) => (
+export const Loader = memo(({ className }: LoaderProps) => (
     <div className={classNames('lds-ripple', {}, [className])}>
         <div />
         <div />
     </div>
-);
+));
