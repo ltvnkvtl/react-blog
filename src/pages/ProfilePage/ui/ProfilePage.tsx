52f598f0a90db127fef96b3a79e89ff5cfd3bcbc
ltvnkvtl <vital.litovk@gmail.com>
2023-03-11 12:15:03 +0400

    feat(profile): profile card, selectors, get data thunk, translations

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -3,7 +3,9 @@ import { useTranslation } from 'react-i18next';
 import {
     DynamicModuleLoader, ReducersList,
 } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { profileReducer } from 'entities/Profile';
+import { fetchProfileData, ProfileCard, profileReducer } from 'entities/Profile';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useEffect } from 'react';
 
 const reducers: ReducersList = {
     profile: profileReducer,
@@ -18,11 +20,16 @@ const ProfilePage = (props: ProfilePageProps) => {
         className,
     } = props;
     const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProfileData());
+    }, [dispatch]);
 
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <div className={classNames('', {}, [className])}>
-                /
+                <ProfileCard />
             </div>
         </DynamicModuleLoader>
     );
